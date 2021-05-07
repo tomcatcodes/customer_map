ecdefd59ba295d007c91d7cebf79b80b3b512daa
tepeters21 <55461551+tepeters21@users.noreply.github.com>
2021-05-07 18:51:10 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+    //TODO: add https support
+    console.trace();
+});
